a81a8c609eb44ada5d2f73e43a91f25471291a6f
Threkork <threkork@hotmail.com>
2024-07-31 23:46:48 +0800

    modified:   src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //！ A **fast and lightweight** OneBot V11 bot framework developed using Rust.
 //！ The project is in beta status, currently featuring **message listening** and **basic API capabilities**.
 //！
-//！ More documentation can be found at [Github-Kovi](#)
+//！ More documentation can be found at [Github-Kovi](https://github.com/Threkork/Kovi)
 
 pub mod bot;
