1599e8603316b6f74dd88e7bf9615f4507066fc5
Threkork <threkork@hotmail.com>
2024-08-16 02:25:35 +0800

    modified:   src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@
 //! The project is in beta status.
 //!
 //! More documentation can be found at [Github-Kovi](https://github.com/Threkork/Kovi) Or [Kovi-doc](https://www.threkork.me/kovi-doc)
+//!
+//! 中文文档或更多文档请查看[Github-Kovi](https://github.com/Threkork/Kovi) 和 [Kovi-doc](https://www.threkork.me/kovi-doc)
 
 
 mod log;
