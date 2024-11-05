04c75a9d7c2b52b90109dc2748ca16f085ef425f
Threkork <threkork@hotmail.com>
2024-11-06 00:16:17 +0800

    modified:   src/mpsc.rs

--- src/mpsc.rs
+++ src/mpsc.rs
@@ -1,16 +1,16 @@
-use ahash::RandomState;
-use parking_lot::Mutex;
-use std::{collections::HashMap, sync::LazyLock};
+// use ahash::RandomState;
+// use parking_lot::Mutex;
+// use std::{collections::HashMap, sync::LazyLock};
 
-pub mod oneshot;
+// pub mod oneshot;
 
-static MPSC_MANAGER: LazyLock<Mutex<HashMap<String, Channel<Box<dyn Any + Send>, RandomState>>>> =
-    LazyLock::new(|| Mutex::new(HashMap::new()));
+// static MPSC_MANAGER: LazyLock<Mutex<HashMap<String, Channel<Box<dyn Any + Send>, RandomState>>>> =
+//     LazyLock::new(|| Mutex::new(HashMap::new()));
 
-pub(crate) struct MpscManaGer {}
+// pub(crate) struct MpscManaGer {}
 
-impl MpscManaGer {
-    pub(crate) fn init() -> Self {
-        todo!()
-    }
-}
+// impl MpscManaGer {
+//     pub(crate) fn init() -> Self {
+//         todo!()
+//     }
+// }
